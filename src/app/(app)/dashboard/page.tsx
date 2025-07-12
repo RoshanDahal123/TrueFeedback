ca179549cf7449a7ffecaac74625864169d96278
RoshanDahal123 <dahalroshan765@gmail.com>
2025-07-12 23:13:01 +0545

    Error:fixed

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,7 +1,158 @@
-import React from "react";
+"use client";
 
-function page() {
-  return <div>Dashboard</div>;
-}
+import { Button } from "@/components/ui/button";
+import { useToast } from "@/hooks/use-toast";
+import { Message } from "@/model/User";
+import { AcceptMessageSchema } from "@/schemas/acceptMessageSchema";
+import { ApiResponse } from "@/types/ApiResponse";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios, { AxiosError } from "axios";
+import { User } from "next-auth";
+import { useSession } from "next-auth/react";
+import { useCallback, useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+
+const page = () => {
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [loading, setIsLoading] = useState<boolean>(false);
+  const [isSwitchLoading, setIsSwitchLoading] = useState<boolean>(false);
+
+  const { toast } = useToast();
+
+  const handleDeleteMessage = (messageId: string) => {
+    setMessages(messages.filter((message) => message._id !== messageId));
+  };
+
+  const { data: session } = useSession();
+
+  const form = useForm({
+    resolver: zodResolver(AcceptMessageSchema),
+  });
+
+  const { register, watch, setValue } = form;
+
+  const acceptMessages = watch("acceptMessages");
+
+  const fetchAcceptMessage = useCallback(async () => {
+    setIsSwitchLoading(true);
+    try {
+      const response = await axios.get<ApiResponse>("/api/accept-messages");
+      setValue("acceptMessages", response.data.isAcceptingMessage ?? false);
+    } catch (error) {
+      const axiosError = error as AxiosError;
+      toast({
+        title: "Error I",
+        description:
+          (axiosError.response?.data as ApiResponse)?.message ||
+          "Failed to fetch message settings",
+        variant: "destructive",
+      });
+    } finally {
+      setIsSwitchLoading(false);
+    }
+  }, [setValue]);
+  const fetchMessages = useCallback(
+    async (refresh: boolean = false) => {
+      setIsLoading(true);
+      setIsSwitchLoading(false);
+      try {
+        const response = await axios.get<ApiResponse>("/api/get-messages");
+        setMessages(response?.data?.messages || []);
+        if (refresh) {
+          toast({
+            title: "refreshed messages",
+            description: "Showing latest messages",
+          });
+        }
+      } catch (error) {
+        const axiosError = error as AxiosError;
+        toast({
+          title: "Error II",
+          description:
+            (axiosError.response?.data as ApiResponse)?.message ||
+            "Failed to fetch message settings",
+          variant: "destructive",
+        });
+      } finally {
+        setIsLoading(false);
+        setIsSwitchLoading(false);
+      }
+    },
+    [setIsLoading, setMessages]
+  );
+
+  useEffect(() => {
+    if (!session || !session.user) return;
+    fetchMessages();
+    fetchAcceptMessage();
+  }, [session, setValue, fetchAcceptMessage, fetchMessages]);
+
+  //handle switch change
+
+  const handleSwitchChange = async () => {
+    try {
+      const response = await axios.post<ApiResponse>("/api/accept-messages", {
+        acceptMessages: !acceptMessages,
+      });
+      setValue("acceptMessages", !acceptMessages);
+      toast({
+        title: response.data.message,
+        variant: "default",
+      });
+    } catch (error) {
+      const axiosError = error as AxiosError;
+      toast({
+        title: "Error III ",
+        description:
+          (axiosError.response?.data as ApiResponse)?.message ||
+          "Failed to update accept-messages",
+        variant: "destructive",
+      });
+    }
+  };
+
+  const { username } = (session?.user as User) || "";
+
+  // Check if we're on the client side before accessing window
+  const baseUrl =
+    typeof window !== "undefined"
+      ? `${window.location.protocol}//${window.location.host}`
+      : "";
+  const profileUrl = `${baseUrl}/u/${username}`;
+
+  const copyToClipboard = () => {
+    if (typeof navigator !== "undefined") {
+      navigator.clipboard.writeText(profileUrl);
+      toast({
+        title: "Url Copied",
+        description: "Profile Url has been copied to clipboard",
+      });
+    }
+  };
+
+  if (!session || !session.user) {
+    return <>Please Login</>;
+  }
+  return (
+    <div className="my-8 mx-4 md:mx-8 lg:mx-auto p-6 bg-white rounded w-full max-w-6xl">
+      <h1 className="text-4xl font-bold mb-4">User Dashboard</h1>
+
+      <div className="mb-4">
+        <h2 className="text-lg font-semibold mb-2">Copy Your Unique Link</h2>{" "}
+        <div className="flex items-center">
+          <input
+            type="text"
+            value={profileUrl}
+            disabled
+            placeholder="Your unique profile URL"
+            aria-label="Profile URL"
+            className="input input-bordered w-full p-2 mr-2"
+          />
+          <Button onClick={copyToClipboard}>Copy</Button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default page;
